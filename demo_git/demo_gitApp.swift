fedae70090072b8b85d255aa8b03ba6c01d8ce16
narendraprajapati10 <narendraprajapati10@gmail.com>
2023-05-19 11:57:58 +0530

    Initial Commit

---
+++ demo_git/demo_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_gitApp.swift
+//  demo_git
+//
+//  Created by WhyQ on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct demo_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
